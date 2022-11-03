d9eab775960da9535894d691cda7bd9d732739dc
Mark <mark.zhytkovich@aventus.work>
2022-11-03 15:42:08 +0300

    Main.java changes

--- Main.java
+++ Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static int age;
+    public static String name;
+
+    public static void printNameAndAge() {
+        age = 22;
+        name = "Alex";
+        System.out.println(name + " " + age);
+    }
+
+    public static void main(String[] args) {
+        printNameAndAge();
+    }
 }
