cc84ec97d03c1055717519b57067743c8af7a895
Mark <mark.zhytkovich@aventus.work>
2022-11-23 19:28:35 +0300

    homework Classes

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String [] args) {
+        Engineer Mike = new Engineer("Mike", "Abrams", "developer", 3000);
+        Mike.displayInfo();
+        Engineer Max = new Engineer("Max", "Smith", "devops", 2500);
+        Max.displayInfo();
+        Engineer Tim = new Engineer("Tim", "Piers", "QA", 2300);
+        Tim.displayInfo();
+
+        Car mercedes = new Car(7, 200,5);
+        mercedes.displayInfo();
+    }
+}
