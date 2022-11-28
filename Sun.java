0bfd16e3e8373e7ad1bbdd903d0fa07be260bf0d
Mark <mark.zhytkovich@aventus.work>
2022-11-28 22:29:38 +0300

    Sun.java

---
+++ Sun.java
@@ -0,0 +1,12 @@
+public class Sun {
+
+    static int count;
+    public static final String OUTPUT_TEXT = "Солнце вставало следующее количество раз: ";
+
+    public static void daybreak (int count) {
+        System.out.println(OUTPUT_TEXT + count);
+    }
+
+
+
+}
