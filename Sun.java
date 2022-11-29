ca601da09b071ec034af7283987cc0561462597d
Mark <mark.zhytkovich@aventus.work>
2022-11-29 11:53:06 +0300

    daybreak method updated

--- Sun.java
+++ Sun.java
@@ -1,12 +1,13 @@
 public class Sun {
 
-    static int count;
+    static int count = 3;
     public static final String OUTPUT_TEXT = "Солнце вставало следующее количество раз: ";
 
-    public static void daybreak (int count) {
-        System.out.println(OUTPUT_TEXT + count);
+    public static void daybreak() {
+        count += 1;
     }
 
-
-
+    public static void sunrisesCount() {
+            System.out.println(OUTPUT_TEXT + count);
+    }
 }
