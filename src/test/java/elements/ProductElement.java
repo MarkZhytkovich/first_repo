6c3e7b796144daba32153f320861b5a4d3a65af2
Mark <mark.zhytkovich@aventus.work>
2023-02-15 17:01:14 +0300

    ProductElement.java updated

--- src/test/java/elements/ProductElement.java
+++ src/test/java/elements/ProductElement.java
@@ -1,6 +1,5 @@
 package elements;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.*;
@@ -8,61 +7,45 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class ProductElement {
 
-    private String productXpath = "//div[@class='inventory_item_name' and contains (text(), '%s')]";
-    private String addToCartXpath = "//button[@id='add-to-cart-%s']";
-    private String removeXpath = "//button[@id='remove-%s']";
+    private String productXpath = "//div[@Class='inventory_item' and contains (., '%s')]";
+    private String productLinkXpath = "//div[@Class='inventory_item' and contains (., '%s')]//a";
+    private String addRemoveButtonXpath = "//div[@Class='inventory_item' and contains (., '%s')]//button";
 
-    private String productName;
-    private String addToCartButtonId;
-    private String removeButtonId;
-    private SelenideElement product;
+    private String productIdentifier;
+    private String addRemoveButtonIdentifier;
+    private SelenideElement productLink;
     private SelenideElement addButton;
     private SelenideElement removeButton;
 
-
-    public ProductElement(String productName, String addToCartButtonId, String removeButtonId) {
-        this.productName = productName;
-        this.addToCartButtonId = addToCartButtonId;
-        this.removeButtonId = removeButtonId;
+    public ProductElement(String productIdentifier) {
+        this.productIdentifier = productIdentifier;
     }
 
     private void initElement() {
-        String locator = String.format(productXpath, productName);
-        product = $x(locator).shouldBe(enabled);
+        String locator = String.format(productLinkXpath, productIdentifier);
+        productLink = $x(locator).shouldBe(enabled);
+        String addButtonLocator = String.format(addRemoveButtonXpath, addRemoveButtonIdentifier);
+        addButton = $x(addButtonLocator).shouldBe(enabled);
+        String removeButtonLocator = String.format(addRemoveButtonXpath, addRemoveButtonIdentifier);
+        removeButton = $x(removeButtonLocator).shouldBe(enabled);
     }
-
-    private void initAddToCart() {
-        String locator = String.format(addToCartXpath, addToCartButtonId);
-        addButton = $x(locator)
-                .shouldBe(visible)
-                .shouldBe(enabled);
-    }
-
-    private void initRemove() {
-        String locator = String.format(removeXpath, removeButtonId);
-        removeButton = $x(locator)
-                .shouldBe(visible)
-                .shouldBe(enabled);
-    }
-
 
     public void openProductDetails() {
         initElement();
-        product.click();
+        productLink.click();
     }
 
     public void addProductToCart() {
-        initAddToCart();
-        addButton.isEnabled();
-        addButton.click();
+        initElement();
+        if (addButton.isDisplayed()) {
+            addButton.click();
+        }
     }
 
     public void removeProductFromCart() {
-        initRemove();
-        removeButton.isEnabled();
-        removeButton.click();
+        initElement();
+       if (removeButton.isDisplayed()) {
+           removeButton.click();
+       }
     }
-
-
-
 }
