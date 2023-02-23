96d2c2fd1b975873bb32fef0e00fefb783f8f348
Mark <mark.zhytkovich@aventus.work>
2023-02-23 19:16:45 +0300

    occupation, MaritalStatus, Education

---
+++ src/test/java/models/Occupation.java
@@ -0,0 +1,18 @@
+package models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Occupation {
+    @SerializedName("1")
+    private String one;
+    @SerializedName("2")
+    private String two;
+    @SerializedName("3")
+    private String three;
+    @SerializedName("4")
+    private String four;
+    @SerializedName("5")
+    private String five;
+    @SerializedName("8")
+    private String eight;
+}
